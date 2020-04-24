ead89a3c3683bfc850e8cd6f4a178360fa16d27d
ThomasMurat <thomasmurat1992@gmail.com>
2020-04-24 17:46:30 +0200

    up exo 3

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let languages = ['html', 'css', 'javascript', 'php', 'mysql', 'c++','ruby','python'];
+var y = languages.length - 1;
+for(i = 0; i <= y; i++) {
+    document.write(languages[i] + "<br>");
+}
